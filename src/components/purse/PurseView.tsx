0cbbd36a5f5fe512d22d4adbe4b071dafc12b2ef
duckietm <troop@mailtjes.com>
2024-07-30 11:09:26 +0200

    :up: :up: Fix he key prop directly to the JSX element for bubble alert & start on purse view

--- src/components/purse/PurseView.tsx
+++ src/components/purse/PurseView.tsx
@@ -4,14 +4,12 @@ import { CreateLinkEvent, GetConfiguration, LocalizeText } from '../../api';
 import { Column, Flex, LayoutCurrencyIcon, Text } from '../../common';
 import { usePurse } from '../../hooks';
 
-export const PurseView: FC<{}> = props =>
-{
-    const { purse = null, hcDisabled = false } = usePurse();
+export const PurseView: FC = () => {
+    const { purse } = usePurse();
     const displayedCurrencies = useMemo(() => GetConfiguration<number[]>('system.currency.types', []), []);
     const currencyDisplayNumberShort = useMemo(() => GetConfiguration<boolean>('currency.display.number.short', false), []);
 
-    const getClubText = useMemo(() =>
-    {
+    const getClubText = useMemo(() => {
         if (!purse) return null;
 
         const totalDays = ((purse.clubPeriods * 31) + purse.clubDays);
@@ -19,59 +17,36 @@ export const PurseView: FC<{}> = props =>
 
         if (purse.clubLevel === HabboClubLevelEnum.NO_CLUB) return LocalizeText('purse.clubdays.zero.amount.text');
 
-        else if ((minutesUntilExpiration > -1) && (minutesUntilExpiration < (60 * 24))) return FriendlyTime.shortFormat(minutesUntilExpiration * 60);
-
-        else return FriendlyTime.shortFormat(totalDays * 86400);
+        return FriendlyTime.shortFormat(
+            (minutesUntilExpiration > -1 && minutesUntilExpiration < 60 * 24) 
+            ? minutesUntilExpiration * 60 
+            : totalDays * 86400
+        );
     }, [purse]);
 
-    const getCurrencyElements = useCallback((offset: number, limit: number = -1, seasonal: boolean = false) =>
-    {
+    const getCurrencyElements = useCallback((offset: number, limit: number = -1) => {
         if (!purse || !purse.activityPoints || !purse.activityPoints.size) return null;
 
-        const types = Array.from(purse.activityPoints.keys()).filter(type => (displayedCurrencies.indexOf(type) >= 0));
+        const types = Array.from(purse.activityPoints.keys()).filter(type => displayedCurrencies.includes(type));
 
-        let count = 0;
-
-        while (count < offset)
-        {
-            types.shift();
-
-            count++;
-        }
-
-        count = 0;
-
-        const elements: JSX.Element[] = [];
-
-        for (const type of types)
-        {
-            if ((limit > -1) && (count === limit)) break;
-
-            if (seasonal) elements.push(<div className='col-md-4 text-center'>
+        const elements = types.slice(offset, limit === -1 ? undefined : offset + limit).map(type => (
+            <div key={type} className='col-md-4 text-center'>
                 <div style={{ display: "inline-block" }}>
                     <b>{purse.activityPoints.get(type)}</b>
                 </div>
                 <LayoutCurrencyIcon type={type} style={{ display: "inline-block", marginLeft: "5px", position: "relative", top: "3px" }} />
-            </div>);
-            else elements.push(<div className='col-md-4 text-center'>
-                <div style={{ display: "inline-block" }}>
-                    <b>{purse.activityPoints.get(type)}</b>
-                </div>
-                <LayoutCurrencyIcon type={type} style={{ display: "inline-block", marginLeft: "5px", position: "relative", top: "3px" }} />
-            </div>);
-
-            count++;
-        }
+            </div>
+        ));
 
         return elements;
-    }, [purse, displayedCurrencies, currencyDisplayNumberShort]);
+    }, [purse, displayedCurrencies]);
 
     if (!purse) return null;
 
     return (
         <Column className="nitro-purse-container" gap={1}>
             <div className="nitro-purse nitro-notification p-2">
-                <div style={{borderBottom: "1px #363636 solid", paddingRight: "5px", paddingBottom: "10px" }}>
+                <div style={{ borderBottom: "1px #363636 solid", paddingRight: "5px", paddingBottom: "10px" }}>
                     <div className='row gx-0'>
                         <div className='col-md-8'>
                             <div className='row gx-0'>
@@ -87,7 +62,7 @@ export const PurseView: FC<{}> = props =>
                         <div className='col-md-4 text-center' style={{ borderLeft: "1px #363636 solid" }}>
                             <LayoutCurrencyIcon style={{ display: "inline-block", marginLeft: "5px", position: "relative", top: "3px" }} className="club-text" type="hc" />
                             <div style={{ display: "inline-block" }}>
-                                <Text variant='white' small bold>Infinito</Text>
+                                <Text variant='white' small bold>{LocalizeText('catalog.club.vip')}</Text>
                             </div>
                         </div>
                     </div>
@@ -95,24 +70,24 @@ export const PurseView: FC<{}> = props =>
                 <div className='mt-2'>
                     <div className='row gx-2'>
                         <div className="col-md-4">
-                            <Flex style={{height: "20px !important"}} center pointer className="nitro-purse-right-button help p-1 w-100" onClick={event => CreateLinkEvent('help/show')}>
+                            <Flex style={{ height: "20px !important" }} center pointer className="nitro-purse-right-button help p-1 w-100" onClick={() => CreateLinkEvent('help/show')}>
                                 <Text variant="white" small>{LocalizeText('help.button.cfh')}</Text>
                             </Flex>
                         </div>
                         <div className="col-md-4">
-                            <Flex style={{height: "20px !important"}} center pointer className="nitro-purse-right-button disconnect p-1 w-100" onClick={event => window.location.href = "/index"}>
+                            <Flex style={{ height: "20px !important" }} center pointer className="nitro-purse-right-button disconnect p-1 w-100" onClick={() => window.location.href = "/index"}>
                                 <i className="icon icon-purse-disconnect" />
                             </Flex>
                         </div>
                         <div className="col-md-4">
-                            <Flex style={{height: "20px !important"}} center pointer className="nitro-purse-right-button settings p-1 w-100" onClick={event => CreateLinkEvent('user-settings/toggle')} >
+                            <Flex style={{ height: "20px !important" }} center pointer className="nitro-purse-right-button settings p-1 w-100" onClick={() => CreateLinkEvent('user-settings/toggle')}>
                                 <i className="icon icon-purse-settings" />
                             </Flex>
                         </div>
                     </div>
                 </div>
             </div>
-            {getCurrencyElements(2, -1, true)}
+            {getCurrencyElements(2)}
         </Column>
     );
-}
+};
