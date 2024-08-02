5f5e1be6d033baf7ef66ae0bf16aaf0fe18a6b46
duckietm <troop@mailtjes.com>
2024-08-02 13:28:28 +0200

    :up: Loading screen

--- src/components/loading/LoadingView.tsx
+++ src/components/loading/LoadingView.tsx
@@ -1,51 +1,50 @@
-import { FC, useCallback, useMemo } from 'react';
-import { GetConfiguration } from '../../api';
-import { Base, Column, LayoutProgressBar, Text } from '../../common';
+import { FC } from "react";
+import { Base, Column, LayoutProgressBar, Text } from "../../common";
 
-interface LoadingViewProps
-{
-    isError: boolean;
-    message: string;
-    percent: number;
+var randomImage = "loadingPhoto position-absolute";
+const randomImg: number = Math.floor(Math.random() * 10);
+
+interface LoadingViewProps {
+  isError: boolean;
+  message: string;
+  percent: number;
 }
 
-export const LoadingView: FC<LoadingViewProps> = props =>
-{
-    const { isError = false, message = '', percent = 0 } = props;
-    
-    const loadingAdvices = GetConfiguration<string>('loading.text');
-    
-    const LoadingAdvice = useCallback(() => 
-    {
-        return (
-            <div className="small w-75 mx-auto">
-                { loadingAdvices ? loadingAdvices[Math.floor(Math.random() * loadingAdvices.length) + 0] : 'Cargando textos...' }
+export const LoadingView: FC<LoadingViewProps> = (props) => {
+  const { isError = false, message = "", percent = 0 } = props;
+
+  return (
+    <Column
+      fullHeight
+      alignItems="center"
+      justifyContent="center"
+      className="nitro-loading"
+    >
+      <div className="loading-stories">
+        <div
+          className="loadingPhoto position-absolute"
+          data-image={randomImg}
+        />
+        <div className="imageOverlay position-absolute"></div>
+      </div>
+      <Column className="text-center py-4">
+        {isError && message && message.length ? (
+          <Base className="fs-4 text-shadow">{message}</Base>
+        ) : (
+          <>
+            <Base className="fs-4 text-shadow">{message}</Base>
+            <div className="nitro-loading-bar mt-2">
+              <div
+                className="nitro-loading-bar-inner"
+                style={{ width: `${percent}%` }}
+              />
             </div>
-        )
-    }, [ loadingAdvices ]);
-
-    const LoadingAdviceCached = useMemo(() => <LoadingAdvice />, [ LoadingAdvice ])
-    
-    return (
-        <Column fullHeight position="relative" className="nitro-loading">
-            <Base fullHeight className="container h-100 advice-height">
-                <Column fullHeight alignItems="center" justifyContent="center">
-                    <div className='nitro-loading-logo' />
-                    <Column size={ 12 } className="text-center" alignItems="center">
-                        { isError && (message && message.length) ?
-                            <Base className="fs-4 text-shadow">{ message }</Base>
-                            :
-                            <>
-                                <LayoutProgressBar progress={ percent } className="large" text={ `Bobba está cargando (${ percent.toFixed() }%)...` } />
-                            </>
-                        }
-                    </Column>
-                    <Base className="advice-loading">
-                        <Text variant="white" fontWeight="bold">Consejos de Bobba</Text>
-                        { LoadingAdviceCached }
-                    </Base>
-                </Column>
-            </Base>
-        </Column>
-    );
-}
+            <div className="percent">
+              <p>{percent.toFixed()}%</p>
+            </div>
+          </>
+        )}
+      </Column>
+    </Column>
+  );
+};
--- src/components/purse/views/CurrencyView.tsx
+++ src/components/purse/views/CurrencyView.tsx
@@ -37,3 +37,4 @@ export const CurrencyView: FC<CurrencyViewProps> = props =>
         </OverlayTrigger>
     );
 }
+	
