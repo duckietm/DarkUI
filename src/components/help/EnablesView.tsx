d2a8ec777d7fd0137df7d7ac41a897032e74776b
duckietm <troop@mailtjes.com>
2024-08-13 08:34:47 +0200

    :up: Added Effectlist

--- src/components/help/EnablesView.tsx
+++ src/components/help/EnablesView.tsx
@@ -1,6 +1,6 @@
 import { ILinkEventTracker } from '@nitrots/nitro-renderer';
 import { FC, useEffect, useState } from 'react';
-import { AddEventLinkTracker, RemoveLinkEventTracker } from '../../api';
+import { AddEventLinkTracker, GetConfiguration, LocalizeText, RemoveLinkEventTracker } from '../../api';
 import { NitroCardContentView, NitroCardHeaderView, NitroCardView } from '../../common';
 import { SanctionSatusView } from './views/SanctionStatusView';
 import { NameChangeView } from './views/name-change/NameChangeView';
@@ -10,32 +10,31 @@ export const EnablesView: FC<{}> = props =>
     const [isVisible, setIsVisible] = useState(false);
     const [effects, setEffects] = useState(null);
 
-    const onClose = () =>
-    {
+    const onClose = () => {
         setIsVisible(false);
     }
 
-    const getEffectsData = () =>
-    {
-        fetch("https://swfs.habbe.es/gamedata/EffectMap.json")
-            .then((response) => response.json())
-            .then((obj) =>
-            {
-                setEffects(obj.effects.filter(obj => !obj.id.includes(".")));
+    const getEffectsData = () => {
+        const effectsUrl = GetConfiguration('enable-list.url') || '';
+
+        fetch(effectsUrl)
+            .then(response => response.json())
+            .then(obj => {
+                setEffects(obj.effects.filter(effect => !effect.id.includes('.')));
+            })
+            .catch(error => {
+                console.error('Error fetching effects:', error);
             });
     }
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         const linkTracker: ILinkEventTracker = {
-            linkReceived: (url: string) =>
-            {
+            linkReceived: (url: string) => {
                 const parts = url.split('/');
 
                 if (parts.length < 2) return;
 
-                switch (parts[1])
-                {
+                switch (parts[1]) {
                     case 'show':
                         setIsVisible(true);
                         getEffectsData();
@@ -56,21 +55,33 @@ export const EnablesView: FC<{}> = props =>
         return () => RemoveLinkEventTracker(linkTracker);
     }, []);
 
-
     return (
         <>
             {isVisible &&
                 <NitroCardView className="nitro-help">
-                    <NitroCardHeaderView headerText="Lista de enables" onCloseClick={onClose} />
-                    <NitroCardContentView className="text-black">
-                        {effects === null ? <div className='alert bg-warning text-white text-center'><h4>Cargando, espera por favor.</h4></div> : (
+                    <NitroCardHeaderView headerText={ LocalizeText('widget.memenu.effects') } onCloseClick={onClose} />
+                    <NitroCardContentView className="text-white bg-black">
+                        {effects === null ? (
+                            <div className='alert bg-warning text-white text-center'>
+                                <h4>{ LocalizeText('navigator.loading') }</h4>
+                            </div>
+                        ) : (
                             <div className='row'>
-                                {effects.length > 0 && effects.map((effect, index) =>
-                                    <div className='col-md-3 text-center' key={index}>
-                                        <img src={`https://imager.bobba.chat/avatarimage/hd-180-1390.hr-989999843-61.ch-215-94.lg-275-62.sh-300-62&gesture=sml&direction=2&effect=${effect.id}&img_format=gif`} />
-                                        <span className='badge bg-dark text-white'>ID: {effect.id}</span>
-                                    </div>
-                                )}
+                                {effects.length > 0 && effects.map((effect, index) => {
+                                    const avatarBaseUrl = GetConfiguration('avatar.loading.url') || '';
+                                    const avatarUrl = `${avatarBaseUrl}hd-180-1390.hr-989999843-61.ch-215-94.lg-275-62.sh-300-62&gesture=sml&direction=2&effect=${effect.id}&img_format=gif`;
+                                    
+                                    return (
+                                        <div className='col-md-4 text-center' key={index}>
+                                            <img
+                                                src={avatarUrl}
+                                                alt={`Effect ${effect.id}`}
+                                                className="img-fluid"
+                                            />
+                                            <span className='badge bg-dark text-white'>Enable ID: {effect.id}</span>
+                                        </div>
+                                    );
+                                })}
                             </div>
                         )}
                     </NitroCardContentView>
