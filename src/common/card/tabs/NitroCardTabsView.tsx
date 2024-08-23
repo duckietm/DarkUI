c564a79a2ad2c5dbdac8edae60e6ac106bad4def
duckietm <troop@mailtjes.com>
2024-08-23 08:54:08 +0200

    :up Fix Card Tabs

--- src/common/card/tabs/NitroCardTabsView.tsx
+++ src/common/card/tabs/NitroCardTabsView.tsx
@@ -1,9 +1,8 @@
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useMemo } from 'react';
 import { Flex, FlexProps } from '../..';
 
 export const NitroCardTabsView: FC<FlexProps> = props =>
 {
-    const [backgroundUrl, setBackgroundUrl] = useState("https://files.habboemotion.com/resources/images/website_backgrounds/02.gif");
     const { justifyContent = 'center', gap = 1, classNames = [], children = null, ...rest } = props;
 
     const getClassNames = useMemo(() =>
@@ -15,13 +14,8 @@ export const NitroCardTabsView: FC<FlexProps> = props =>
         return newClassNames;
     }, [ classNames ]);
 
-    useEffect(() => {
-        var localStorage = window.localStorage;
-        if(localStorage.getItem("tabs-background") != null) setBackgroundUrl(localStorage.getItem("tabs-background"));
-    }, [])    
-
     return (
-        <div style={{background: "rgba(24, 24, 24, 0.6)"}}>
+        <div style={{ background: "rgba(24, 24, 24, 0.6)" }}>
             <Flex justifyContent={ justifyContent } gap={ gap } classNames={ getClassNames } { ...rest }>
                 { children }
             </Flex>
